7276c99962230f78ec4cb35ee52f0111ab9bc6c2
Le Xing <73104004+LeXing1105@users.noreply.github.com>
2024-01-30 11:48:42 +0000

    Add files via upload

---
+++ Arduino/autoencoder_Arduino.h
@@ -0,0 +1,7 @@
+#ifndef TENSORFLOW_LITE_MICRO_MODEL_TEST_H_
+#define TENSORFLOW_LITE_MICRO_MODEL_TEST_H_
+
+extern const unsigned char autoencoder_Arduino[]; 
+extern const int autoencoder_Arduino_len;
+
+#endif  
