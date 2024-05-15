5d812f5cbf910c59b33849900d8dbc513c31f7c6
Le Xing <73104004+LeXing1105@users.noreply.github.com>
2024-05-15 15:02:18 +0100

    Delete Arduino directory

--- Arduino/autoencoder_Arduino.h
+++
@@ -1,7 +0,0 @@
-#ifndef TENSORFLOW_LITE_MICRO_MODEL_TEST_H_
-#define TENSORFLOW_LITE_MICRO_MODEL_TEST_H_
-
-extern const unsigned char autoencoder_Arduino[]; 
-extern const int autoencoder_Arduino_len;
-
-#endif  
