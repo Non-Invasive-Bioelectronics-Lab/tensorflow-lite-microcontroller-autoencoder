a73837cc88380bee732ec2113056a0ba8bdf1c74
Mahdi <48448186+MSH19@users.noreply.github.com>
2024-07-24 19:54:56 +0100

    Created a folder for autoencoder_arduino.ino

--- autoencoder_coral_micro/autoencoder_arduino/autoencoder_arduino.ino
+++ autoencoder_coral_micro/autoencoder_arduino/autoencoder_arduino.ino
@@ -1,7 +1,7 @@
-#include <TensorFlowLite.h>
-#include "autoencoder_int8.h"
-#include <Arduino_LSM9DS1.h>
 
+///////////////////////////////////////////////////////////////////// include libraries 
+// tensorflow lite libraries 
+#include <TensorFlowLite.h>
 #include "tensorflow/lite/micro/all_ops_resolver.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/micro/micro_log.h"
@@ -9,19 +9,25 @@
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 
+// the definition of the auto-encoder model 
+#include "autoencoder_int8.h"
+// #include <Arduino_LSM9DS1.h>
 
+// define the size of the input data 
 const int segment_size = 800;
+// create an array of float numbers to hold the input segement 
 float input_data[segment_size];
-// Define a fixed seed value for the random number generator
+// define a fixed seed value for the random number generator
 const unsigned long fixedSeed = 123;
 
-
 // Globals 
-namespace {
+namespace 
+{
+
 const tflite::Model* model = nullptr;
+
 tflite::MicroInterpreter* interpreter = nullptr;
-// TfLiteTensor* input = nullptr;
-// TfLiteTensor* output = nullptr;
+
 // Define input and output tensors
 TfLiteTensor* input = interpreter->input(0);
 TfLiteTensor* output = interpreter->output(0);
@@ -31,23 +37,24 @@ TfLiteTensor* output = interpreter->output(0);
 // Create an area of memory to use for input, output, and other TensorFlow arrays.
 // You'll need to adjust this by compiling, running, and looking for errors.
 constexpr int kTensorArenaSize = 100*1024;
+
 // Keep aligned to 16 bytes for CMSIS
 // alignas(16) uint8_t tensor_arena[kTensorArenaSize];
 uint8_t tensor_arena[kTensorArenaSize];
+
 }  // namespace
 
-
-
-
-void setup() {
+void setup() 
+{
 
   tflite::InitializeTarget();
 
   Serial.begin(9600);  // Initialize serial communication
    
-  //Load model
+  // Load model
   model = tflite::GetModel(autoencoder_int8);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
+  if (model->version() != TFLITE_SCHEMA_VERSION) 
+  {
     MicroPrintf(
         "Model provided is schema version %d not equal "
         "to supported version %d.",
@@ -70,21 +77,24 @@ void setup() {
     return;
   }
 
-
   // Check input tensor
-  if (input->dims->size != 2 || input->dims->data[0] != 1 || input->dims->data[1] != 800) {
+  if (input->dims->size != 2 || input->dims->data[0] != 1 || input->dims->data[1] != 800) 
+  {
     Serial.println("Input tensor shape mismatch.");
     return;
   }
+
   Serial.println("Model loaded and tensor allocated successfully");
   
 }
 
 
-void loop() {
+void loop() 
+{
   randomSeed(fixedSeed);
   float synthetic_data[800];
-  for (int i = 0; i < 800; i++) {
+  for (int i = 0; i < 800; i++) 
+  {
     synthetic_data[i] = random(0, 100);  // Generate random values (adjust range as needed)
   }
 
@@ -92,21 +102,29 @@ void loop() {
   int data_length = sizeof(synthetic_data) / sizeof(synthetic_data[0]);
   float min_val = synthetic_data[0];
   float max_val = synthetic_data[0];
-  for (int i = 1; i < data_length; i++) {
-    if (synthetic_data[i] < min_val) {
+  
+  for (int i = 1; i < data_length; i++) 
+  {
+    if (synthetic_data[i] < min_val) 
+    {
       min_val = synthetic_data[i];
     }
-    if (synthetic_data[i] > max_val) {
+    if (synthetic_data[i] > max_val) 
+    {
       max_val = synthetic_data[i];
     }
   }
   float normalized_data[800];
-  for (int i = 0; i < data_length; i++) {
+  for (int i = 0; i < data_length; i++) 
+  {
     normalized_data[i] = (synthetic_data[i] - min_val) / (max_val - min_val);
   }
+  
   // Print the generated array
   Serial.begin(9600);
-  for (int i = 0; i < 800; i++) {
+  
+  for (int i = 0; i < 800; i++) 
+  {
     Serial.print(normalized_data[i]);
   }
 
